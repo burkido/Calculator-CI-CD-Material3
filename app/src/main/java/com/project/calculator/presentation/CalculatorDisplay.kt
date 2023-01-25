3830ef577949c8855bc2b4c119a562d1368f1928
Burak Karaduman <karadumanburak000@gmail.com>
2023-01-25 20:38:34 +0300

    - Bug fixed in status bar - Created values.xml for night mode

--- app/src/main/java/com/project/calculator/presentation/CalculatorDisplay.kt
+++ app/src/main/java/com/project/calculator/presentation/CalculatorDisplay.kt
@@ -1,16 +1,20 @@
 package com.project.calculator.presentation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -33,6 +37,34 @@ fun CalculatorDisplay(
             singleLine = true,
             cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
             modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(name = "LightMode", showBackground = true,)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true, name = "DarkMode")
+@Composable
+fun CalculatorDisplayPreviewLight() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        CalculatorDisplay(
+            expression = "345",
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .clip(
+                    RoundedCornerShape(
+                        bottomStart = 25.dp,
+                        bottomEnd = 25.dp
+                    )
+                )
+                .background(MaterialTheme.colorScheme.secondaryContainer)
+                .padding(
+                    vertical = 64.dp,
+                    horizontal = 16.dp
+                )
         )
     }
 }
